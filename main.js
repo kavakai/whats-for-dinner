584b63c6c99e23825abcfe91fc4e6a623fc97e8f
Kai Kuller <kavakai@gmail.com>
2021-10-29 12:53:16 -0600

    Add variables to access html elements

---
+++ main.js
@@ -0,0 +1,13 @@
+var addRecipe = document.querySelector('.add-recipe-button');
+
+var side = document.querySelector('#sideChoice');
+
+var main = document.querySelector('#mainChoice');
+
+var dessert = document.querySelector('#dessertChoice');
+
+var makeMeal = document.querySelector('#entireMeal');
+
+var letsCook = document.querySelector('.meal-button');
+
+var potImage = document.querySelector('#cookpot');
