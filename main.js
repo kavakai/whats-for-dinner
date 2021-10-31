11a1856a0a92d3d0efd4c0cf27aebc74620a0cfa
Kai Kuller <kavakai@gmail.com>
2021-10-31 17:22:29 -0600

    Refactor JS for submission

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var addRecipe = document.querySelector('.add-recipe-button');
 
-
 var side = document.querySelector('#sideChoice');
 
 var main = document.querySelector('#mainChoice');
@@ -13,13 +12,13 @@ var letsCook = document.querySelector('.meal-button');
 
 var potImage = document.querySelector('.pot');
 
-var pot = document.querySelector('#cookpot')
+var pot = document.querySelector('#cookpot');
 
 var clearButton = document.querySelector('.clear');
 
 var makeThis = document.querySelector('.make-this');
 
-var dishSelection = document.querySelector('#meal-here')
+var dishSelection = document.querySelector('.meal-display');
 
 
 var sideSelection = [
@@ -72,76 +71,38 @@ var dessertSelection = [
 'Eclairs',
 ];
 
-// target radio buttons to capture input
-// create eventListener-click to capture input values
-// get random item from selected array
-// hide cookpot image
-// display choice in cookpots place
-// show clear button
 
-// radioButtons.addEventListener('click', selectArray)
+letsCook.addEventListener('click', showMeal);
 
-letsCook.addEventListener('click', showMeal)
+clearButton.addEventListener('click', goHome);
 
-clearButton.addEventListener('click', goHome)
-
-
-// function selectArray() {
-//   for (var i = 0; i < radioButtons.length; i++)
-//   if (radioButtons[i].checked) {
-//
-//   }
-// }
 
 function showMeal() {
   clearButton.classList.remove('hidden');
   makeThis.classList.remove('hidden');
-  pot.classList.add('hidden')
-  dishSelection.classList.remove('hidden')
-   displayMeal()
-}
+  pot.classList.add('hidden');
+  dishSelection.classList.remove('hidden');
+    displayMeal();
+};
 
 function displayMeal() {
   if (side.checked) {
-    dishSelection.innerText = sideSelection[getRandomIndex(sideSelection)]
-} else if (main.checked) {
-  dishSelection.innerText = mainSelection[getRandomIndex(mainSelection)]
-} else if (dessert.checked) {
-  dishSelection.innerText = dessertSelection[getRandomIndex(dessertSelection)]
-} else if (makeMeal.checked) {
-  dishSelection.innerText = sideSelection[getRandomIndex(sideSelection)] + mainSelection[getRandomIndex(mainSelection)] + dessertSelection[getRandomIndex(dessertSelection)]
-}
-
-
-}
+    dishSelection.innerText = sideSelection[getRandomIndex(sideSelection)] + "!";
+  } else if (main.checked) {
+    dishSelection.innerText = mainSelection[getRandomIndex(mainSelection)] + "!";
+  } else if (dessert.checked) {
+    dishSelection.innerText = dessertSelection[getRandomIndex(dessertSelection)] + "!";
+  } else if (makeMeal.checked) {
+    dishSelection.innerText = mainSelection[getRandomIndex(mainSelection)] + " with a side of " + sideSelection[getRandomIndex(sideSelection)] + " and " + dessertSelection[getRandomIndex(dessertSelection)] + " for dessert!";
+  }
+};
 
 function goHome() {
-  clearButton.classList.add('hidden')
-  makeThis.classList.add('hidden')
-  pot.classList.remove('hidden')
-  dishSelection.classList.add('hidden')
-}
-// radioButtons.addEventListener('checked', getValue);
-//
-// function getValue(event) {
-//   selectedType = '';
-//   for (var i = 0; i < radioButtons.length; i++)
-//   if (radioButtons[i].target.event) {
-//      selectedType = radioButtons[i].value
-//   }
-//   event.preventDefault()
-// }
-//
-// letsCook.addEventListener('click', displayCoice)
-//
-//
-// function displayCoice(selectedType) {
-//   if (selectedType === side) {
-//     potImage.innerHTML = sideSelection[RandomIndex(sideSelection)]
-//   }
-// }
-
-
+  clearButton.classList.add('hidden');
+  makeThis.classList.add('hidden');
+  pot.classList.remove('hidden');
+  dishSelection.classList.add('hidden');
+};
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
